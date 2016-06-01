02c33144975e3f5104231041676e794d60c1499e
csaszarhunor <csaszarhunor@gmail.com>
2016-06-01 09:16:33 +0200

    javascript refactor

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 /**
  * Created by csasz on 5/26/2016.
  */
-(function(){
-    var validateContactData = function(){
+(function(validation){
+    validation.validateContactData = function(){
         var name = document.getElementsByName("name")[0];
         var email = document.getElementsByName("email")[0];
         var message = document.getElementsByName("message")[0];
         var sendBttn = document.getElementById("sendBttn");
 
-        if (validateName(name) && validateEmail(email) && validateMessage(message)){
+        if (validation.validateName(name) && validation.validateEmail(email) && validation.validateMessage(message)){
             sendBttn.disabled = false;
         }
     };
 
-    var validateName = function(name){
+    validation.validateName = function(name){
         var isValid = name.value.length > 3 && name.value.indexOf(' ') > -1;
         if (!isValid){
             name.style.borderColor = "red";
@@ -24,12 +24,13 @@
         return isValid;
     };
 
-    var isSameEnding = function(str, suffix) {
+    validation.isSameEnding = function(str, suffix) {
         return str.indexOf(suffix, str.length - suffix.length) !== -1;
     };
 
-    var validateEmail = function(email){
-        var isValid = email.value.indexOf('@') > 0 && (isSameEnding(email.value, '.hu') || isSameEnding(email.value, '.com'));
+    validation.validateEmail = function(email){
+        var isValid = email.value.indexOf('@') > 0 &&
+            (validation.isSameEnding(email.value, '.hu') || validation.isSameEnding(email.value, '.com'));
         if (!isValid){
             email.style.borderColor = "red";
         }
@@ -39,7 +40,7 @@
         return isValid;
     };
 
-    var validateMessage = function(message){
+    validation.validateMessage = function(message){
         var isValid = 20 < message.value.length && message.value.length < 500;
         if (!isValid){
             message.style.borderColor = "red";
@@ -50,7 +51,7 @@
         return isValid;
     };
 
-    var sendMessage = function(){
+    validation.sendMessage = function(){
         var name = document.getElementsByName("name")[0];
         var email = document.getElementsByName("email")[0];
         var message = document.getElementsByName("message")[0];
@@ -58,8 +59,8 @@
         alert("To:\n" + name.value + "\n" + email.value + "\n\nMessage:\n" + message.value + "\n\n" + "Your message has been sent!")
     };
 
-    document.getElementById("sendBttn").addEventListener("click",sendMessage);
-    document.getElementsByName("name")[0].addEventListener('change', validateContactData);
-    document.getElementsByName("email")[0].addEventListener('change', validateContactData);
-    document.getElementsByName("message")[0].addEventListener('change', validateContactData);
-})();
+    document.getElementById("sendBttn").addEventListener("click",validation.sendMessage);
+    document.getElementsByName("name")[0].addEventListener('change', validation.validateContactData);
+    document.getElementsByName("email")[0].addEventListener('change', validation.validateContactData);
+    document.getElementsByName("message")[0].addEventListener('change', validation.validateContactData);
+})(window.validation = window.validation || {});
